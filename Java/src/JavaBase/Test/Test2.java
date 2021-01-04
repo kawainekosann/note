b83b6b0e0367ab8f76bc23f716ca942949c75878
FEIMAO <1532921016@qq.com>
2021-01-05 01:32:32 +0800

    Java相关

---
+++ Java/src/JavaBase/Test/Test2.java
@@ -0,0 +1,9 @@
+package JavaBase.Test;
+public class Test2 {
+    public static void main(String[] args) {
+        Test test = new Test();
+        test.bite();
+        //test.bite4();
+        test.bite5();
+    }
+}

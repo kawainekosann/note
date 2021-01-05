ee81359a1dbb1aedacfdba9d0683291ae8b217c4
FEIMAO <1532921016@qq.com>
2021-01-06 00:30:18 +0800

    Java相关

--- Java/src/JavaBase/Static/Test3.java
+++ Java/src/JavaBase/Static/Test3.java
@@ -15,6 +15,7 @@ public class Test3 extends Test {
     public static void main(String[] args) {
         Test.bite3();
         Test test = new Test();
+        //test.bite4();
         //test.bite5();
         Test3 test3 = new Test3();
         test3.bite5();
